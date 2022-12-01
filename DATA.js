1dd6b182a275a68fcd4738fcca77fb5fecb882a4
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-01 11:15:54 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,35 @@
+export const item = {
+    "id": "1c93dcf2-70b8-4199-bd5e-0d8a2765e42e",
+    "chat_list": [
+        {
+            "id": "dc7dbf36-1691-4250-8a8a-b0d00e8bd9a2",
+            "sender_profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png",
+            "sender_message": "Try trial large if center. Individual similar about sure service.",
+            "your_message": "Bed oil ability American. Tend tax loss play impact whose large court."
+        },
+        {
+            "id": "c677df7c-5cd3-415c-bb0f-19e3cea8cb9f",
+            "sender_profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/58.png",
+            "sender_message": "West do reason enough reflect order. Type score opportunity drop much.",
+            "your_message": "Soon heart up also discover stop rise. Drive agent even arm view instead student."
+        },
+        {
+            "id": "c86a132d-09fc-4dca-ba3a-25d26e378feb",
+            "sender_profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png",
+            "sender_message": "Force part land green industry position. Eye fact end outside nor beautiful. Child prove shoulder here ever win expect.",
+            "your_message": "Work itself increase wide interesting. Item relationship spend box among majority. Behind trade life approach TV."
+        },
+        {
+            "id": "b4e074b6-008a-4d7a-8538-e14eb5ec9ddf",
+            "sender_profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png",
+            "sender_message": "Employee nor him against.",
+            "your_message": "Best million often myself. Final that build."
+        },
+        {
+            "id": "44239bcf-6cb2-4a79-b417-db1e72d2fa19",
+            "sender_profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png",
+            "sender_message": "Explain cause specific every probably view stock. Father exactly cup standard gas special. Summer remember material town.",
+            "your_message": "Face catch test. Down on reach weight near late yes half. Down work up step dog relationship when. During sign century law."
+        }
+    ]
+}
